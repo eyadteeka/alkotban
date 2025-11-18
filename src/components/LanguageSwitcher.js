9b42bd6aad2d2dfae49fcc94822f7772e1f52a5d
ahmedomran631 <ahmedomran631@gmail.com>
2025-11-18 18:56:38 +0200

    update

--- src/components/LanguageSwitcher.js
+++ src/components/LanguageSwitcher.js
@@ -3,10 +3,11 @@ import { useTranslation } from 'react-i18next';
 
 const LanguageSwitcher = () => {
   const { i18n } = useTranslation();
-
   const toggleLanguage = () => {
     const newLang = i18n.language === 'en' ? 'ar' : 'en';
     i18n.changeLanguage(newLang);
+        document.documentElement.dir = i18n.language === 'ar' ? 'rtl' : 'ltr';
+    document.documentElement.lang = i18n.language;
   };
 
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
-import LanguageSwitcher from './LanguageSwitcher';
+import React from "react";
+import { useTranslation } from "react-i18next";
+import LanguageSwitcher from "./LanguageSwitcher";
 
 const Navbar = () => {
   const { t } = useTranslation();
@@ -8,29 +8,61 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4 px-lg-5 py-3 py-lg-0">
       <a href="" className="navbar-brand p-0">
-        <h1 className="text-primary m-0">
-          <img className="img-fluid" src="img/Co-logos/atilalbg.png" alt={t('navbar_logo_alt')} />
-          {t('navbar_brand_name')}
-        </h1>
+        <h5 className="text-primary m-0">
+          <img
+            className="img-fluid"
+            style={{ paddingLeft: "10px", paddingRight: "10px" }}
+            src="img/Co-logos/atilalbg.png"
+            alt={t("navbar_logo_alt")}
+          />
+          {t("navbar_brand_name")}
+        </h5>
       </a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarCollapse"
+      >
         <span className="fa fa-bars"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarCollapse">
         <div className="navbar-nav ms-auto py-0 pe-4">
-          <a href="index.html" className="nav-item nav-link active">{t('nav_home')}</a>
-          <a href="about.html" className="nav-item nav-link">{t('nav_about')}</a>
-          <a href="service.html" className="nav-item nav-link">{t('nav_services')}</a>
-          <a href="menu.html" className="nav-item nav-link">{t('nav_menu')}</a>
+          <a href="index.html" className="nav-item nav-link active">
+            {t("nav_home")}
+          </a>
+          <a href="about.html" className="nav-item nav-link">
+            {t("nav_about")}
+          </a>
+          <a href="service.html" className="nav-item nav-link">
+            {t("nav_services")}
+          </a>
+          <a href="menu.html" className="nav-item nav-link">
+            {t("nav_menu")}
+          </a>
           <div className="nav-item dropdown">
-            <a href="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">{t('nav_pages')}</a>
+            <a
+              href="#"
+              className="nav-link dropdown-toggle"
+              data-bs-toggle="dropdown"
+            >
+              {t("nav_pages")}
+            </a>
             <div className="dropdown-menu m-0">
-              <a href="booking.html" className="dropdown-item">{t('nav_booking')}</a>
-              <a href="team.html" className="dropdown-item">{t('nav_team')}</a>
-              <a href="testimonial.html" className="dropdown-item">{t('nav_testimonials')}</a>
+              <a href="booking.html" className="dropdown-item">
+                {t("nav_booking")}
+              </a>
+              <a href="team.html" className="dropdown-item">
+                {t("nav_team")}
+              </a>
+              <a href="testimonial.html" className="dropdown-item">
+                {t("nav_testimonials")}
+              </a>
             </div>
           </div>
-          <a href="contact.html" className="nav-item nav-link">{t('nav_contact')}</a>
+          <a href="contact.html" className="nav-item nav-link">
+            {t("nav_contact")}
+          </a>
         </div>
         <div className="d-flex align-items-center gap-3">
           <LanguageSwitcher />
